b4456e84748b141ae4ddb324e2bc65c21e499af8
github username <github email address>
2017-09-18 16:25:41 -0400

    Done

--- app.rb
+++ app.rb
@@ -3,4 +3,48 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   # Write your code here!
 
+  get '/reversename/:name' do
+    @name = params[:name]
+    @name = @name.reverse
+    "#{@name}"
+  end
+
+  get "/square/:number" do
+    @num = params[:number].to_i
+    @square = @num * @num
+    "#{@square}"
+  end
+
+  get "/say/:number/:phrase" do
+    @phrase = params[:phrase]
+    @n = params[:number].to_i
+    "#{@phrase * @n}"
+  end
+
+  get "/say/:word1/:word2/:word3/:word4/:word5" do
+    @word1 = params[:word1]
+    @word2 = params[:word2]
+    @word3 = params[:word3]
+    @word4 = params[:word4]
+    @word5 = params[:word5]
+    "#{@word1 + " " + @word2 + " " + @word3 + " " + @word4 + " " + @word5 + "."}"
+  end
+
+  get "/:operation/:number1/:number2" do
+    @operation = params[:operation]
+    @number1 = params[:number1].to_i
+    @number2 = params[:number2].to_i
+    if @operation == "add"
+      "#{@number1 + @number2}"
+    elsif @operation == "subtract"
+      "#{@number1 - @number2}"
+    elsif @operation == "multiply"
+      "#{@number1 * @number2}"
+    elsif @operation == "divide"
+      "#{@number1 / @number2}"
+    else
+      "That is not a valid operator"
+    end
+  end
+
 end
